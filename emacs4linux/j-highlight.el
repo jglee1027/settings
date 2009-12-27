8b96ca65c4902d0c16cfd583856b45f6aa14fe66
Jonggyu Lee <jglee1027@gmail.com>
2009-12-28 00:24:14 +0900

    to improve isearch speed

--- emacs4linux/j-highlight.el
+++ emacs4linux/j-highlight.el
@@ -2,34 +2,37 @@
   "*Put symbol at current point into search string."
   (interactive)
   (let ((sym (symbol-at-point)))
-    (if sym
+    (if (and sym
+			 (not (string= sym j-highlight-symbol-old-symbol)))
         (progn
           (setq isearch-regexp t
                 isearch-string (concat "\\_<" (regexp-quote (symbol-name sym)) "\\_>")
                 isearch-message (mapconcat 'isearch-text-char-description isearch-string "")
-                isearch-yank-flag t))
+                isearch-yank-flag t
+				j-highlight-symbol-old-symbol sym))
       (ding)))
   (isearch-search-and-update))
 
 ;; variable for the timer object
-(defvar j-highlight-timer-timer nil)
+(defvar j-highlight-symbol-timer nil)
+(defvar j-highlight-symbol-old-symbol nil)
 
 ;; callback function 
-(defun j-highlight-timer-callback ()
+(defun j-highlight-symbol-callback ()
   (save-excursion
 	(isearch-yank-symbol)))
 
 ;; start functions
-(defun j-highlight-timer-run-toggle ()
+(defun j-highlight-symbol-run-toggle ()
   (interactive)
-  (if (timerp j-highlight-timer-timer)
+  (if (timerp j-highlight-symbol-timer)
 	  (progn
-		(cancel-timer j-highlight-timer-timer)
-		(message "j-highlight-timer off.")
-		(setq j-highlight-timer-timer nil))
+		(cancel-timer j-highlight-symbol-timer)
+		(message "j-highlight-symbol off.")
+		(setq j-highlight-symbol-timer nil))
 	(progn
-	  (setq j-highlight-timer-timer
-			(run-with-idle-timer 1.5 t 'j-highlight-timer-callback))
-	  (message "j-highlight-timer on."))))
+	  (setq j-highlight-symbol-timer
+			(run-with-idle-timer 1.5 t 'j-highlight-symbol-callback))
+	  (message "j-highlight-symbol on."))))
 
-(define-key global-map (kbd "C-x j h") 'j-highlight-timer-run-toggle)
+(define-key global-map (kbd "C-x j h") 'j-highlight-symbol-run-toggle)
