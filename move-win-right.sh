cc9b49476b1c83d7f0f53d96080687c84b20a92a
Jonggyu Lee <jglee1027@gmail.com>
2011-01-06 03:14:15 +0900

    add move-win-left.sh, move-win-right.sh move-win-up.sh

---
+++ move-win-right.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+WIDTH=`xdpyinfo | grep 'dimensions:' | cut -f 2 -d ':' | cut -f 1 -d 'x'` && HALF=$(($WIDTH/2)) && wmctrl -r :ACTIVE: -b add,maximized_vert && wmctrl -r :ACTIVE: -e 0,$HALF,0,$HALF,-1
