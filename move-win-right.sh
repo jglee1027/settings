f8f1eff37a08aaf698d8acb7cd0bcc7a7efa3be2
Jonggyu Lee <jglee1027@gmail.com>
2011-01-06 16:11:01 +0900

    improve move-win-left.sh move-win-right.sh
    can move the current active window to left or right of not only the
    total screen but also the monitor in which the current active window
    exists

--- move-win-right.sh
+++ move-win-right.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-WIDTH=`xdpyinfo | grep 'dimensions:' | cut -f 2 -d ':' | cut -f 1 -d 'x'` && HALF=$(($WIDTH/2)) && wmctrl -r :ACTIVE: -b add,maximized_vert && wmctrl -r :ACTIVE: -e 0,$HALF,0,$HALF,-1
+MONITORS=`xrandr -q | grep -e '\<connected\>' | wc -l`
+ACTIVE_WIN_ID=`xprop -root | grep -e '^_NET_ACTIVE_WINDOW' | awk '{print $5}'`
+ACTIVE_WIN_POS_X=`xwininfo -id $ACTIVE_WIN_ID  | grep 'Absolute upper-left X' | cut -f 2 -d ':'`
+
+WIDTH_OF_SCREEN=`xdpyinfo | grep 'dimensions:' | cut -f 2 -d ':' | cut -f 1 -d 'x'`
+HALF_OF_SCREEN=$(($WIDTH_OF_SCREEN / 2))
+HALF_OF_MONITOR=$(($HALF_OF_SCREEN / $MONITORS))
+
+# right in a monitor
+if [ $ACTIVE_WIN_POS_X -ge $HALF_OF_SCREEN ]; then
+	X=$(($HALF_OF_SCREEN + $HALF_OF_MONITOR))
+else
+	X=$HALF_OF_MONITOR
+fi
+WIDTH=$HALF_OF_MONITOR
+
+# right in the screen
+if [ "$1" = "monitor" ]; then
+	X=$HALF_OF_SCREEN
+	WIDTH=$HALF_OF_SCREEN
+fi
+
+wmctrl -r :ACTIVE: -b add,maximized_vert && wmctrl -r :ACTIVE: -e 0,$X,0,$WIDTH,-1
