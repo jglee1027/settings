44007dcf9a0b99c2c61cc738fd408e12a17f0bce
Jonggyu Lee <jglee1027@gmail.com>
2015-08-23 10:08:08 +0900

    modify autostart.sh for openbox
    * change desktop background color
    * add conky, tilda and x-www-browser

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,1 +1,16 @@
+# Set a background color
+
+SETROOT=""
+if which hsetroot >/dev/null; then
+	SETROOT=hsetroot
+elif which esetroot >/dev/null; then
+	SETROOT=esetroot
+elif which xsetroot >/dev/null; then
+	SETROOT=xsetroot
+fi
+test -z $SETROOT || $SETROOT -solid "#203040"
+
 tint2&
+conky&
+tilda&
+x-www-browser&
