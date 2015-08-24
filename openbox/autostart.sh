5493cb988262cec17e4a44c2d79e1ccd17892bd9
Jonggyu Lee <jglee1027@gmail.com>
2015-08-24 18:39:36 +0900

    can use autostart-custom.sh on openbox

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Set a background color
 
 SETROOT=""
@@ -9,6 +11,11 @@ elif which xsetroot >/dev/null; then
 	SETROOT=xsetroot
 fi
 test -z $SETROOT || $SETROOT -solid "#203040"
+
+OPENBOX_CONFIG_DIR=$(dirname $0)
+if [ -f $OPENBOX_CONFIG_DIR/autostart-custom.sh ]; then
+	$OPENBOX_CONFIG_DIR/autostart-custom.sh
+fi
 
 tint2&
 conky&
