7095ff7d7b65a87c621fb299f3ef3692eaea4429
Jonggyu Lee <jglee1027@gmail.com>
2018-08-18 02:39:52 +0900

    modify .emacs-custom.el

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -1,6 +1,29 @@
 (global-hl-line-mode t)
 
-(ignore-errors
-  (el-get-init 'switch-window)
-  (el-get-init 'grep-a-lot)
-  (el-get-init 'magit))
+(ignore-errors (el-get-init 'grep-a-lot))
+(ignore-errors (el-get-init 'switch-window))
+(ignore-errors (el-get-init 'magit))
+(ignore-errors (el-get-init 'lua-mode)
+               (load-library "lua-mode"))
+(ignore-errors (el-get-init 'cmake-mode)
+               (load-library "cmake-mode")
+               (setq cmake-tab-width 4))
+(ignore-errors (el-get-init 'markdown-mode)
+               (load-library "markdown-mode"))
+(ignore-errors (el-get-init 'highlight-sexp)
+               (load-library "highlight-sexp"))
+(ignore-errors (el-get-init 'c-eldoc)
+               (load-library "c-eldoc"))
+
+;; slime
+(ignore-errors (el-get-init 'slime)
+               (setq inferior-lisp-program "/usr/bin/clisp")
+               (setq slime-contribs '(slime-fancy)))
+
+;; cmake-ide
+(ignore-errors (el-get-init 's))
+(ignore-errors (el-get-init 'rtags))
+(ignore-errors (el-get-init 'cmake-ide)
+               (require 'rtags)
+               (require 'cmake-ide)
+               (cmake-ide-setup))
