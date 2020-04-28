14b8c9511506be3932562a35b9d0858b8a471d82
Jonggyu Lee <jglee1027@gmail.com>
2020-04-28 17:19:20 +0900

    add conky_network_all() in network.lua on conky
    conky_network_all() function displays all linked interfaces

--- conky/network.lua
+++ conky/network.lua
@@ -15,19 +15,37 @@ function os.capture(cmd, raw)
    return s
 end
 
-function conky_network(iface_type, iface)
+function conky_network(ifname)
    local res = ""
-   local iface = os.capture("ip link | grep 'state UP' | grep " .. iface .. "| cut -d ' ' -f 2 | sed s/:// ")
+   local stat = os.capture("ip link | grep 'state UP' | grep \"" .. ifname .. ":\" | cut -d ' ' -f 2 | sed s/:// ")
 
-   if iface  == "" then
-      res = res.format("${color #0077ff}%-8s IP: ${color}", iface_type) .. "DOWN"
-      return res
+   if stat == "" then
+      -- only display ifnames that it's state is up
+      return
    end
 
-   local ssid = os.capture("/sbin/iwgetid --raw " .. iface)
+   local ssid = os.capture("/sbin/iwgetid --raw " .. ifname)
 
-   res = res.format("${color #0077ff}%-8s IP: ${color}${addrs ", iface_type) .. iface .. "} " .. ssid .. "\n" ..
-      "${color #0077ff} Net Down:$color ${downspeed " .. iface .. "}k/s${alignr}${color #0077ff}Net Up:$color ${upspeed " .. iface .. "}k/s\n" ..
-      "${color #0077ff} ${downspeedgraph " .. iface .. " 24,155 104E8B 0077ff} $alignr${color #0077ff}${upspeedgraph " .. iface .. " 24,155 104E8B 0077ff}"
+   res = res.format("${color #0077ff}%-10s IP: ${color}${addrs ", ifname) .. ifname .. "} " .. ssid .. "\n" ..
+      "${color #0077ff} Net Down:$color ${downspeed " .. ifname .. "}k/s${alignr}${color #0077ff}Net Up:$color ${upspeed " .. ifname .. "}k/s\n" ..
+      "${color #0077ff} ${downspeedgraph " .. ifname .. " 24,155 104E8B 0077ff} $alignr${color #0077ff}${upspeedgraph " .. ifname .. " 24,155 104E8B 0077ff}"
+   return res
+end
+
+function conky_network_all()
+   local res = ""
+   local ifnames = os.capture("ls /sys/class/net")
+   for ifname in string.gmatch(ifnames, "%S+") do
+      local stat = os.capture("cat /sys/class/net/" .. ifname .. "/operstate")
+      if stat == "up" then
+         local str = conky_network(ifname)
+         if str ~= nil then
+            if res ~= "" then
+               res = res .. "\n"
+            end
+            res = res .. str
+         end
+      end
+   end
    return res
 end
