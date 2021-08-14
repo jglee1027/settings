751382a264921bdfe0d7ed656a3b7ddcc90676dd
Jonggyu Lee <jglee1027@gmail.com>
2021-08-14 15:36:52 +0900

    use melpa-stable and melpa(unstable) archives

--- emacs/init.el
+++ emacs/init.el
@@ -31,9 +31,7 @@ Otherwise, return result of last form in BODY."
 
 ;;;; melpa
 (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (ignore-errors
