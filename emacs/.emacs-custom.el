dd9642240e0782fb905ffb448a540032bb366659
Jonggyu Lee <jglee1027@gmail.com>
2021-11-04 02:02:47 +0900

    set doc-view-resolution to 300

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -1,5 +1,6 @@
 (setq default-font-spec-eng (font-spec :family "envypn" :size 19.0))
 (setq default-font-spec-kor (font-spec :family "NanumMyeongjo" :size 19.0))
+(setq doc-view-resolution 300)
 (setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
 (set-face-foreground 'org-hide "#ffffff")
 (when window-system
