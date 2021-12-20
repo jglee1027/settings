7c85af56bae50cef1a684e49fb9bd7a262c4878d
Jonggyu Lee <jglee1027@gmail.com>
2021-12-20 19:39:29 +0900

    use ox-confluence from http://orgmode.org/elpa

--- emacs/init.el
+++ emacs/init.el
@@ -37,6 +37,7 @@ Otherwise, return result of last form in BODY."
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -567,6 +568,12 @@ Otherwise, return result of last form in BODY."
   ;; (mu4e-views-mu4e-use-view-msg-method "html") ;; select the default
   (setq mu4e-views-next-previous-message-behaviour 'stick-to-current-window) ;; when pressing n and p stay in the current window
   (setq mu4e-views-auto-view-selected-message t)) ;; automatically open messages when moving in the headers view
+
+(use-package org
+  :pin org
+  :ensure org-plus-contrib
+  :config
+  (require 'ox-confluence))
 
 (use-package org-bullets
   :ensure t
