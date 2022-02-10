da27fb3c15400bfdc9f20975a17d75720c655b94
Jonggyu Lee <jglee1027@gmail.com>
2022-02-10 13:22:02 +0900

    make .emacs-custom.el portable

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -1,7 +1,13 @@
-(setq default-font-spec-eng (font-spec :family "envypn" :size 19.0))
-(setq default-font-spec-kor (font-spec :family "NanumSquareRound" :size 19.0))
+(cond ((equal system-type 'darwin)
+       (setq default-font-spec-eng (font-spec :family "Monaco" :size 19.0))
+       (setq default-font-spec-kor (font-spec :family "AppleGothic" :size 19.0))
+       (setq org-plantuml-jar-path "/usr/local/Cellar/plantuml/1.2022.1/libexec/plantuml.jar"))
+      (t
+       (setq default-font-spec-eng (font-spec :family "envypn" :size 19.0))
+       (setq default-font-spec-kor (font-spec :family "NanumSquareRound" :size 19.0))
+       (setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")))
+
 (setq doc-view-resolution 300)
-(setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
 (set-face-foreground 'org-hide "#ffffff")
 (when window-system
   (setq init-ui-hook '((lambda ()
