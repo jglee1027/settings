be6a1b3ec73bc3657d46dcfa53b72c8b10bc1746
Jonggyu Lee <jglee1027@gmail.com>
2022-04-23 21:43:49 +0900

    set default-font-spec and plantuml jar path on windows

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -2,9 +2,13 @@
        (setq default-font-spec-eng (font-spec :family "Monaco" :size 19.0))
        (setq default-font-spec-kor (font-spec :family "AppleGothic" :size 19.0))
        (setq org-plantuml-jar-path "/usr/local/Cellar/plantuml/1.2022.1/libexec/plantuml.jar"))
+      ((equal system-type 'windows-nt)
+       (setq default-font-spec-eng (font-spec :family "Consolas" :size 19.0))
+       (setq default-font-spec-kor (font-spec :family "Consolas" :size 19.0))
+       (setq org-plantuml-jar-path "c:/Program Files/plantuml/plantuml-1.2022.4.jar"))
       (t
        (setq default-font-spec-eng (font-spec :family "envypn" :size 19.0))
-       (setq default-font-spec-kor (font-spec :family "NanumSquareRound" :size 19.0))
+       (setq default-font-spec-kor (font-spec :family "NanumGothic" :size 19.0))
        (setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")))
 
 (setq doc-view-resolution 300)
