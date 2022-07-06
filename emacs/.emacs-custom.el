ddb2c8e54fad4677e0d6e696a17f5575faf10923
Jonggyu Lee <jglee1027@gmail.com>
2022-07-06 23:59:53 +0900

    rescale NanumGothic font to display the table nicely on Windows

--- emacs/.emacs-custom.el
+++ emacs/.emacs-custom.el
@@ -3,8 +3,12 @@
        (setq default-font-spec-kor (font-spec :family "AppleGothic" :size 19.0))
        (setq org-plantuml-jar-path "/usr/local/Cellar/plantuml/1.2022.1/libexec/plantuml.jar"))
       ((equal system-type 'windows-nt)
-       (setq default-font-spec-eng (font-spec :family "Consolas" :size 19.0))
-       (setq default-font-spec-kor (font-spec :family "Consolas" :size 19.0))
+       ;; (setq default-font-spec-eng (font-spec :family "나눔고딕_AndaleMono" :size 11.0))
+       ;; (setq default-font-spec-kor (font-spec :family "나눔고딕_AndaleMono" :size 11.0))
+       (setq default-font-spec-eng (font-spec :family "Consolas" :size 13.0))
+       (setq default-font-spec-kor (font-spec :family "나눔고딕" :size 13.0))
+       (setq face-font-rescale-alist
+             '(("나눔고딕" . 1.17)))
        (setq org-plantuml-jar-path "c:/Program Files/plantuml/plantuml-1.2022.4.jar"))
       (t
        (setq default-font-spec-eng (font-spec :family "envypn" :size 19.0))
